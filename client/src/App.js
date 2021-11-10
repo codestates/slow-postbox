7736279b9b6d829d1903d28f40f2c769ff9b240c
lim-yubin <yubineric@gmail.com>
2021-11-10 18:30:48 +0900

    Design: 로그인페이지 목업구현
    와이어프레임과 동일하게 목업구현
    
    Issue #14

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+// import React from 'react';
+
+// function App() {
+//   return (
+//     <div>App</div>
+//   );
+// }
+
+// export default App;
+
+
 import React from 'react';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div>App</div>
+    <Login />
   );
 }
 
