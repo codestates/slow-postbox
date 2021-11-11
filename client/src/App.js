107df8883760b2f578e242ded146e17b244ba5f0
lim-yubin <yubineric@gmail.com>
2021-11-11 17:09:53 +0900

    Feat: 로그인페이지 목업구현
    와이어프레임과 동일하게 목업구현
    
    Issue #14

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
-// import React from 'react';
-
-// function App() {
-//   return (
-//     <div>App</div>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
-import Login from './pages/Login';
 
 function App() {
   return (
-    <Login />
+    <div>App</div>
   );
 }
 
 export default App;
+
+
