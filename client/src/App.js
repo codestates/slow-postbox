f1fc01a438c31973b7bcf2f7add1d4ccdcecc18e
lim-yubin <yubineric@gmail.com>
2021-11-12 00:42:43 +0900

    quote> Feat: 회원가입페이지 목업구현 quote> quote> 와이어프레임과 동일하게 목업구현 quote> quote> Issue #18 quote> quote>

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,8 @@
-// import React from 'react';
-
-// function App() {
-//   return <div>App</div>;
-// }
-
-// export default App;
-
-
 import React from 'react';
-import SignUp from './pages/SignUp';
 
 function App() {
-  return <SignUp />
+  return <div>App</div>;
 }
 
 export default App;
-
 
