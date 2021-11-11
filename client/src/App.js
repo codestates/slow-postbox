5b3d3ca23922fd0ec8a1b12c6e2a772a3cdec273
lim-yubin <yubineric@gmail.com>
2021-11-11 17:07:43 +0900

    to save

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+// import React from 'react';
+
+// function App() {
+//   return (
+//     <div>App</div>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
-    <div>App</div>
+    <SignUp />
   );
 }
 
