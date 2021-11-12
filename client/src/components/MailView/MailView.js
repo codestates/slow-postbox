1522f8cbd1ab2105d8496581a2d72e7987a156e7
moonseonyeong <seon_moon@naver.com>
2021-11-12 23:08:09 +0900

    Design: WholeMailBox CSS수정

--- client/src/components/MailView/MailView.js
+++ client/src/components/MailView/MailView.js
@@ -3,11 +3,9 @@ import './MailView.css'
 import { GoKebabVertical } from "react-icons/go";
 import { FaTimes } from "react-icons/fa"
 import { useState } from 'react';
-import './MailViewModal.css'
-
 
 export default function MailView() {
-  const dummydata4 = [{ id: 1, title: "제목제1111111111111111111111", writerName: "문선영", created_at: "2021.12.31", received_at: "2021.12.31", isChecked: true, isRead: false, content: "컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 " }]
+  const dummydata4 = [{ id: 1, title: " 메일 뷰 css 구현하기", writerName: "문선영", created_at: "2021.12.31", received_at: "2021.12.31", isChecked: true, isRead: false, content: "컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 " }]
   const { id, content, isChecked, isRead, created_at, received_at, title, writerName } = dummydata4[0];
   const [isOpen, setIsOpen] = useState(false)
 
@@ -20,22 +18,22 @@ export default function MailView() {
       <div className="mailview-container">
         <div className="mailview-grid">
 
-          <div> {title} </div>
-          <div> 받는사람 : {writerName} </div>
+          <div className="mailview-title"> {title} </div>
+          <div className="mailview-receive-name"> 받는사람 : {writerName} </div>
           <div> 보낸날짜 : {created_at} / 도착날짜 : {received_at} </div>
-          <div className="modal-flex">
-            <div className="modal-sort" onClick={ModalOnOff} > <GoKebabVertical /> </div>
+          <div className="modal-flex" style={{ border: "none" }}>
+            <div className="modal-sort" >
+              <GoKebabVertical onClick={ModalOnOff} />
 
+            </div>
             {isOpen === true
               ? <MailViewModal ModalOnOff={ModalOnOff} />
               : ""
             }
-
-            <div className="mail-text"> {content} </div>
+            <div className="mail-text" > {content} </div>
           </div>
-          <Link to="WholeSentMail">
-            <button> 닫기 </button>
-          </Link>
+          {/* <button className="btn_close_l"> 닫기 </button> */}
+          <button className="btn_close_m"> 닫기 </button>
         </div>
       </div>
 
@@ -46,7 +44,7 @@ export default function MailView() {
 
 
 function MailViewModal({ ModalOnOff }) {
-  const dummydata4 = [{ id: 1, title: "제목제1111111111111111111111", writerName: "문선영", created_at: "2021.12.31", received_at: "2021.12.31", isChecked: true, isRead: false, content: "컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 컨텐트 " }]
+  const dummydata4 = [{ writerName: "문선영" }]
   const { writerName } = dummydata4[0]
   const testname = "김소현"
 
@@ -62,8 +60,8 @@ function MailViewModal({ ModalOnOff }) {
                 <div className="btn-Modalclose" >
                   <FaTimes onClick={ModalOnOff} />
                 </div>
-                <div><button>답장</button></div>
-                <div><button>삭제</button></div>
+                <div><button className="btn_close_s">답장</button></div>
+                <div><button className="btn_close_s">삭제</button></div>
               </>
             )
             : (
@@ -71,7 +69,9 @@ function MailViewModal({ ModalOnOff }) {
                 <div className="btn-Modalclose" >
                   <FaTimes onClick={ModalOnOff} />
                 </div>
-                <div><button>삭제</button></div>
+                <div>
+                  <button className="btn_close_s">삭제</button>
+                </div>
               </>
             )
           }
