6f76b2ff7cf85382935527b8d238526bad19df67
lim-yubin <yubineric@gmail.com>
2021-11-13 03:37:06 +0900

    Design: Login/SignUp/CompleteSignUp/FindUserInfo 페이지 CSS수정
    와이어프레임의 디자인 기반으로 구현되어있던 목업페이지들의
    CSS수정작업
    
    Issue #52 #56 #57 #108

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import Login from './pages/Login'
+import FindUserInfo from './pages/FindUserInfo'
+import SignUp from './pages/SignUp'
+import CompleteSignUp from './components/SignUp/CompleteSignUp'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Login />
+      <FindUserInfo />
+      <SignUp />
+      <CompleteSignUp />
+    </>
   );
 }
 
--- client/src/components/SignUp/CompleteSignUp.js
+++ client/src/components/SignUp/CompleteSignUp.js
@@ -3,6 +3,9 @@ import './CompleteSignUp.css';
 export default function CompleteSignUp() {
   return (
     <div className='completeSignUp-container'>
+      <div className='title'>
+        <img className='completeSignUp-title' src='img/Logintitle.png'></img>
+      </div>
       <div className='completeSignUp-message'>
         느린우체통 가입을 축하합니다.
       </div>
@@ -10,6 +13,7 @@ export default function CompleteSignUp() {
       <div className='button-section'>
         <button className='login-button'>로그인</button>
       </div>
+      <div className='bottom-section'></div>
     </div>
   );
 }
