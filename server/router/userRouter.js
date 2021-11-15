41218a2b43af5ad8d43fc215df1bb988bceb259d
lim-yubin <yubineric@gmail.com>
2021-11-16 03:04:06 +0900

    Feat: SignUp 페이지 기능구현
    회원가입페이지 기능구현(이메일-인증여부확인/ 비밀번호-암호화필요/ 이름)
    상기 3가지 정보로 회원가입 진행
    oauth/admin/이메일인증시간/비밀번호암호화 기능 추가 예정
    
    Issue #36

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -1,25 +1,25 @@
 const {
-    auth,
-    duplication,
-    kakaologin,
-    login,
-    logout,
-    mailverify,
-    modify,
-    signup,
-    withdraw
-  } = require('../controllers/user');
-  const express = require('express');
-  const router = express.Router();
-  
-  router.get('/auth', auth);
-  router.get('/duplication', duplication);
-  router.post('/kakaologin', kakaologin);
-  router.post('/login', login);
-  router.post('/logout', logout);
-  router.post('/mailverify', mailverify);
-  router.patch('/modify', modify);
-  router.post('/signup', signup);
-  router.delete('withdraw', withdraw);
-  
-  module.exports = router;
+  auth,
+  duplication,
+  kakaologin,
+  login,
+  logout,
+  mailverify,
+  modify,
+  signup,
+  withdraw
+} = require('../controllers/user');
+const express = require('express');
+const router = express.Router();
+
+router.get('/auth', auth);
+router.get('/duplication', duplication);
+router.post('/kakaologin', kakaologin);
+router.post('/login', login);
+router.post('/logout', logout);
+router.post('/mailverify', mailverify);
+router.patch('/modify', modify);
+router.post('/signup', signup);
+router.delete('withdraw', withdraw);
+
+module.exports = router;
