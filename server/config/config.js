da814f2060a4326f0ff0b09f407d5329f114df22
GWANGUIAN <bbaa3218@gmail.com>
2021-11-18 15:11:43 +0900

    temp

--- server/config/config.js
+++ server/config/config.js
@@ -3,10 +3,10 @@ dotenv.config();
 
 const config = {
   development: {
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: 'slow-postbox.c0drf5eriebm.ap-northeast-2.rds.amazonaws.com',
+    user: 'admin',
+    password: 'password',
+    database: 'slow_postbox',
     waitForConnections: true,
     connectionLimit: 1000,
     queueLimit: 0,
