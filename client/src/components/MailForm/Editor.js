522acdc4c769a48b8387505c03fb3765a5d4ecc6
GWANGUIAN <bbaa3218@gmail.com>
2021-11-18 18:33:21 +0900

    Fix: multer 적용

--- client/src/components/MailForm/Editor.js
+++ client/src/components/MailForm/Editor.js
@@ -3,6 +3,10 @@ import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 //import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 import './Editor.css';
+
+
+
+
 function Editor({ formInfo, setFormInfo }) {
   const handleCkeditorState = (evnet, editor) => {
     const data = editor.getData();
@@ -13,6 +17,42 @@ function Editor({ formInfo, setFormInfo }) {
     console.log(data);
   };
 
+  function uploadAdapter(loader) {
+    return {
+      upload: () => {
+        return new Promise((resolve, reject) => {
+          const body = new FormData();
+          loader.file.then((file) => {
+            body.append("files", file);
+            // let headers = new Headers();
+            // headers.append("Origin", "http://localhost:3000");
+            fetch(`${process.env.REACT_APP_SERVER_API}/upload_files`, {
+              method: "post",
+              body: body
+              // mode: "no-cors"
+            })
+              .then((res) => res.json())
+              .then((res) => {
+                resolve({
+                  default: `${process.env.REACT_APP_SERVER_API}/${res.filename}`
+                });
+              })
+              .catch((err) => {
+                reject(err);
+              });
+          });
+        });
+      }
+    };
+  }
+
+
+  function uploadPlugin(editor) {
+    editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+      return uploadAdapter(loader);
+    };
+  }
+
   return (
     <>
       <CKEditor
@@ -20,9 +60,7 @@ function Editor({ formInfo, setFormInfo }) {
         data='<p>Hello from slow postbox!</p>'
         onChange={handleCkeditorState}
         config={{
-          ckfinder: {
-            uploadUrl: `${process.env.REACT_APP_SERVER_API}/uploads`,
-          },
+          extraPlugins: [uploadPlugin]
         }}
         className='ck-editor__ediable'
       />
